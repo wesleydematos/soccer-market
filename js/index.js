39924754d495f79118b92a13f0b65dbd8ada2428
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-20 17:01:14 -0300

    atualização carrinho

--- js/index.js
+++ js/index.js
@@ -5,9 +5,15 @@ ulCarrinho.innerHTML = `<p id="carrinhoVazio">Carrinho Vazio</p><p id="AdicioneI
 function listarProdutos(listaProdutos, secao){
     secao.innerHTML = ""
     for(let i = 0; i<listaProdutos.length; i++){
-        let produto        = listaProdutos[i]
-        let cardProduto    = criarCardProduto(produto)
-        secao.appendChild(cardProduto)
+        let produto             = listaProdutos[i]
+        let cardProduto         = criarCardProduto(produto)
+        let cardProdutoCarrinho = criarCardProdutoCarrinho(produto)
+
+        if(secao != ulCarrinho){
+            secao.appendChild(cardProduto)
+        }else{
+            secao.appendChild(cardProdutoCarrinho)
+        }
     }
 }
 listarProdutos(data, ulProdutos)
@@ -54,4 +60,72 @@ function criarCardProduto(produto){
     tagLi.append(tagDivImg, tagDivContent)
 
     return tagLi
+}
+
+function criarCardProdutoCarrinho(produto){
+
+    let id              = produto.id
+    let img             = produto.img
+    let nome            = produto.nameItem
+    let valor           = produto.value
+  
+    let tagLi           = document.createElement("li")
+    let tagDivImg       = document.createElement("div")
+    let tagImg          = document.createElement("img")
+    let tagDivContent   = document.createElement("div")
+    let tagH2Nome       = document.createElement("h2")
+    let tagSpanPreco    = document.createElement("span")
+    let tagBtnComprar   = document.createElement("button")
+
+    tagDivImg.classList.add("image_product")
+    tagImg.src = `${img}`
+    tagImg.alt = `${nome}`
+    tagDivImg.appendChild(tagImg)
+
+    tagDivContent.classList.add("content_product")
+    tagH2Nome.innerText = `${nome}`
+    tagSpanPreco.classList.add("preco")
+    tagSpanPreco.innerText = `R$ ${valor},00`
+    tagBtnComprar.id =  `${id}`
+    tagBtnComprar.type = "button"
+    tagBtnComprar.innerText = "Remover do Carrinho"
+  
+    tagDivImg.appendChild(tagImg)
+    tagDivContent.append(tagH2Nome, tagSpanPreco, tagBtnComprar)
+    
+    tagLi.append(tagDivImg, tagDivContent)
+
+    return tagLi
+}
+
+
+ulProdutos.addEventListener("click", interceptandoProduto)
+
+let carrinhoCompras = []
+
+function interceptandoProduto(event){
+
+    let btnComprar  = event.target
+    
+    if(btnComprar.tagName == "BUTTON"){
+        let idProduto = btnComprar.id
+        let produto = data.find(function(produto){
+
+            if(produto.id == idProduto){
+                return produto
+            }
+            
+        })
+        adicionarCarrinho(produto)
+    }
+}
+
+function adicionarCarrinho(produto){
+
+    if(produto !== undefined){
+        carrinhoCompras.push(produto)
+
+        listarProdutos(carrinhoCompras, ulCarrinho)
+    }
+ 
 }
