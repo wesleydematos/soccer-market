9502d676aa97e389947c65273a9d95855ef94bd3
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-20 20:15:54 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let ulProdutos       = document.querySelector(".ulProdutos")
 let ulCarrinho       = document.querySelector(".ulCarrinho")
 ulCarrinho.innerHTML = `<p id="carrinhoVazio">Carrinho Vazio</p><p id="AdicioneItens">Adicione Itens</p>`
+document.querySelector(".totalPreco").innerText = "R$ 0,00"
+document.querySelector(".quantidade").innerText = "0"
 
 function listarProdutos(listaProdutos, secao){
     secao.innerHTML = ""
@@ -64,7 +66,6 @@ function criarCardProduto(produto){
 
 function criarCardProdutoCarrinho(produto){
 
-    let id              = produto.id
     let img             = produto.img
     let nome            = produto.nameItem
     let valor           = produto.value
@@ -86,7 +87,6 @@ function criarCardProdutoCarrinho(produto){
     tagH2Nome.innerText = `${nome}`
     tagSpanPreco.classList.add("preco")
     tagSpanPreco.innerText = `R$ ${valor},00`
-    tagBtnComprar.id =  `${id}`
     tagBtnComprar.type = "button"
     tagBtnComprar.innerText = "Remover do Carrinho"
   
@@ -108,6 +108,7 @@ function interceptandoProduto(event){
     let btnComprar  = event.target
     
     if(btnComprar.tagName == "BUTTON"){
+    
         let idProduto = btnComprar.id
         let produto = data.find(function(produto){
 
@@ -117,6 +118,7 @@ function interceptandoProduto(event){
             
         })
         adicionarCarrinho(produto)
+        calcularTotal(carrinhoCompras)
     }
 }
 
@@ -128,4 +130,94 @@ function adicionarCarrinho(produto){
         listarProdutos(carrinhoCompras, ulCarrinho)
     }
  
+}
+
+function removerProduto(event){
+    let btnRemover = event.target
+
+    if(btnRemover.tagName == "BUTTON"){
+        for(let i = 0; i < carrinhoCompras.length; i++){
+            btnRemover.id = `${i}`
+        }
+       
+        let index = btnRemover.id
+        console.log(index)
+        
+        carrinhoCompras.splice(index, 1)
+        carrinhoCompras.innerText = ""
+        listarProdutos(carrinhoCompras, ulCarrinho)
+        calcularTotal(carrinhoCompras)
+    }
+}
+ulCarrinho.addEventListener("click", removerProduto)
+
+function calcularTotal(array){
+    let quantidade = array.length
+    let total      = 0
+
+    for(let i = 0; i < array.length; i++){
+        let valor = array[i].value
+        total += valor
+    }
+
+    document.querySelector(".totalPreco").innerText = `R$ ${total},00`
+    document.querySelector(".quantidade").innerText = `${quantidade}`
+}
+
+let finalizarCompra = document.querySelector("#finalizarCompra")
+finalizarCompra.addEventListener("click", function(){
+    alert("Compra efetuada com sucesso!")
+    document.querySelector(".ulCarrinho").innerHTML = `<p id="carrinhoVazio">Carrinho Vazio</p> <p id="AdicioneItens">Adicione Itens</p>`
+    carrinhoCompras.splice(0, carrinhoCompras.length)
+    document.querySelector(".totalPreco").innerText = "R$ 0,00"
+    document.querySelector(".quantidade").innerText = "0"
+})
+
+
+let inputBusca  = document.querySelector("form input")
+let btnBusca    = document.querySelector("form button")
+
+inputBusca.addEventListener("keyup", function(event){
+    
+    if(event.keyCode == 13){
+
+        let pesquisaUsuario = inputBusca.value
+
+        let resultadoBusca  = busca(pesquisaUsuario)
+
+        listarProdutos(resultadoBusca, ulProdutos)
+
+    }
+})
+
+btnBusca.addEventListener("click", function(event){
+    let btnBuscar  = event.target
+    
+    if(btnBuscar.tagName == "BUTTON"){
+        let pesquisaUsuario = inputBusca.value
+        let resultadoBusca  = busca(pesquisaUsuario)
+
+        listarProdutos(resultadoBusca, ulProdutos)
+
+    }
+})
+
+function busca(valorPesquisa){
+    
+    let resultBusca = []
+
+    for(let i = 0; i<data.length; i++){
+        
+        let pesquisa    = valorPesquisa.toLowerCase()
+        let nomeProduto = data[i].nameItem.toLowerCase()
+        let categoria = data[i].tag[0].toLowerCase()
+        
+        if(nomeProduto.includes(pesquisa) || categoria.includes(pesquisa)){
+            
+            resultBusca.push(data[i])
+        
+        }
+    }
+
+    return resultBusca
 }
